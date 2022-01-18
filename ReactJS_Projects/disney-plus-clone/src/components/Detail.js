5370cb11bfd254b6c7230b9ede635f291a45c55d
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-01-18 13:34:48 +0000

    disney-clone detail titleImg

--- ReactJS_Projects/disney-plus-clone/src/components/Detail.js
+++ ReactJS_Projects/disney-plus-clone/src/components/Detail.js
@@ -101,11 +101,9 @@ const MovieTitle = styled.div`
     min-height: 150px;
     min-width: 200px;
     margin-top: 60px;
+    padding-bottom: 24px;
     
     img{
-        max-width:600px;
-        min-width:200px;
-        width:35vw;
         height:100%;
         object-fit: contain;
     }
