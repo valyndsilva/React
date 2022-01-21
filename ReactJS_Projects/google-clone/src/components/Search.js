2cf62808bd4b9f9be723fd92b4addfb3fcebef26
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-01-21 18:30:24 +0000

    Google-clone Reducer, StateProvider, ContextAPI

--- ReactJS_Projects/google-clone/src/components/Search.js
+++ ReactJS_Projects/google-clone/src/components/Search.js
@@ -4,14 +4,21 @@ import SearchIcon from "@mui/icons-material/Search";
 import MicIcon from "@mui/icons-material/Mic";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { useStateValue } from "../StateProvider";
+import { actionTypes } from "../reducer";
 
 function Search(hideButtons = false) {
+  const [{}, dispatch] = useStateValue("");
   const [input, setInput] = useState("");
   const navigate = useNavigate();
   const search = (e) => {
     e.preventDefault();
     console.log("You hit the search button and typed:", input);
-    navigate("/search");
+    dispatch({
+      type: actionTypes.SET_SEARCH_TERM,
+      term: input,
+    });
+    navigate("/searchresults");
   };
   return (
     <Container>
