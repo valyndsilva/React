7a0c7c703669ecfb8feb5f751a309523ec5993e4
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-02-04 03:29:53 +0000

    clean up css

--- ReactJS_Projects/netflix-app-clone/src/App.js
+++ ReactJS_Projects/netflix-app-clone/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import "./index.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 // import Register from "./pages/register/Register";
