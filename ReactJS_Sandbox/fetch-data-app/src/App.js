46b56b18da9d025e8caccaf811b3562d9c59aff9
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-05-08 06:01:52 +0100

    fetch-data-app

---
+++ ReactJS_Sandbox/fetch-data-app/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import * as ROUTES from './routes/routes';
+import { Home } from './pages';
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route exact path={ROUTES.HOME} element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
