286aafbc385a1b738c36906d5dfcdad2efe2072f
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-05-12 10:56:48 +0100

    Axios API Requests setup

--- ReactJS_Sandbox/react-router-blog-app/src/App.js
+++ ReactJS_Sandbox/react-router-blog-app/src/App.js
@@ -13,33 +13,33 @@ import { useState, useEffect } from 'react';
 import * as ROUTES from './routes/routes';
 import { format } from 'date-fns';
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: 'Post 1',
-      datetime: 'January 01, 2022',
-      body: 'lorem ipsum dolor sit amet, consectet',
-    },
-    {
-      id: 2,
-      title: 'Post 2',
-      datetime: 'January 01, 2022',
-      body: 'lorem ipsum dolor sit amet, consectet',
-    },
-    {
-      id: 3,
-      title: 'Post 3',
-      datetime: 'January 01, 2022',
-      body: 'lorem ipsum dolor sit amet, consectet',
-    },
-    {
-      id: 4,
-      title: 'Post 4',
-      datetime: 'January 01, 2022',
-      body: 'lorem ipsum dolor sit amet, consectet',
-    },
-  ]);
-
+  // const [posts, setPosts] = useState([
+  //   {
+  //     id: 1,
+  //     title: 'Post 1',
+  //     datetime: 'January 01, 2022',
+  //     body: 'lorem ipsum dolor sit amet, consectet',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Post 2',
+  //     datetime: 'January 01, 2022',
+  //     body: 'lorem ipsum dolor sit amet, consectet',
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'Post 3',
+  //     datetime: 'January 01, 2022',
+  //     body: 'lorem ipsum dolor sit amet, consectet',
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'Post 4',
+  //     datetime: 'January 01, 2022',
+  //     body: 'lorem ipsum dolor sit amet, consectet',
+  //   },
+  // ]);
+  const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [postTitle, setPostTitle] = useState('');
