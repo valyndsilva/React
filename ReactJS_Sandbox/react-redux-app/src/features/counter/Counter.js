80a441812b8a3a5d713d7982db09374fee2f0306
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-05-16 11:48:54 +0100

    react-redux-toolkit state actions payload

--- ReactJS_Sandbox/react-redux-app/src/features/counter/Counter.js
+++ ReactJS_Sandbox/react-redux-app/src/features/counter/Counter.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import { increment, decrement, reset, incrementByAmount } from './counterSlice';
+import { useState } from 'react';
 export default function Counter() {
   const count = useSelector((state) => state.counter.count);
   // we can pass any actions created through dispatch()
   const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState(0);
+  const addValue = Number(incrementAmount) || 0; // shortcircuiting ensuring the value is a number or 0 if number isn't in the input, and doesn't return NaN
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
   return (
     <section>
       <p>{count}</p>
@@ -15,6 +23,17 @@ export default function Counter() {
         <button className="button" onClick={() => dispatch(decrement())}>
           -
         </button>
+      </div>
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset</button>
       </div>
     </section>
   );
