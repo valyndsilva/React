74e0566107e7d8a8710a7c4be31a8dd01c4a0ffa
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-05-18 13:21:42 +0100

    update

--- ReactJS_Projects/netflix-app-clone/src/hooks/index.js
+++
@@ -1,3 +0,0 @@
-export { default as useGenre } from "./useGenre";
-export { default as useAuthListener } from "./useAuthListener";
-export { default as useContent } from "./useContent";
--- ReactJS_Projects/netflix-app-clone/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-export { default as Home } from "./home";
-export { default as Browse } from "./browse";
-export { default as Signin } from "./signin";
-export { default as Signup } from "./signup";
-export { default as Search } from "./search";
-export { default as Watch } from "./watch";
-export { default as Trending } from "./trending";
-export { default as Movies } from "./movies";
-export { default as Series } from "./series";
