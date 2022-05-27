8496faba634933d556c2a0a3e46228fa1ce31210
[valyn dsilva] <[valyndsilva@gmail.com]>
2022-05-27 10:18:01 +0100

    add interceptors to attach JWT tokens ->  axiosPrivate

--- react-register-login-authenticate-sandbox/react-login-auth-jwt-access-refresh-tokens-cookies-axios/src/api/axios.js
+++ react-register-login-authenticate-sandbox/react-login-auth-jwt-access-refresh-tokens-cookies-axios/src/api/axios.js
@@ -1,5 +1,14 @@
 import axios from 'axios';
 
+const BASE_URL = 'http://localhost:3500';
+
+// Here we have 2 identical axios instances.
 export default axios.create({
-  baseURL: 'http://localhost:3500',
+  baseURL: BASE_URL,
+});
+
+// Below we attach 'interceptors' to the axiosPrivate that allows to attach JWT tokens for us and even retry when we get a failed request the 1st time. The failure returns status code 403 error which is Forbidden.
+// The interceptors work with the JWT token to refresh the token if our initial request is denied due to an expired token. This works in the background and won't impact users in the app (they won't see anything) and will keep everything secure and will continue to refresh the tokens on a set schedule.
+export const axiosPrivate = axios.create({
+  baseURL: BASE_URL,
 });
